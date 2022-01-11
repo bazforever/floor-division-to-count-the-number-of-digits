c16506dcae6d21937e9c56f88943d365392d9c38
Momo Baz <64978845+bazforever@users.noreply.github.com>
2022-01-11 10:21:50 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,53 @@
+
+# we use the floor division to count the number of digits in a number
+count=0
+number = 75969
+
+while number !=0:
+    number //=10
+    count = count + 1
+
+    print("number od digits is", str(count))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
